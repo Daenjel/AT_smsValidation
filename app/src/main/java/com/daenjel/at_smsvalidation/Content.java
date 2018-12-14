bd8ffd4012a54331f3d9e5b95b4300490dfce550
Daenjel <daenjelkirigo@gmail.com>
2018-12-14 18:09:51 +0300

    creating a class to respond to the verification code

---
+++ app/src/main/java/com/daenjel/at_smsvalidation/Content.java
@@ -0,0 +1,14 @@
+package com.daenjel.at_smsvalidation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class Content extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content);
+    }
+}
